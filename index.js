4d4cee33353450b3db8aff476a820607237f62df
LittleNanonymous <158363151+LittleNanonymous@users.noreply.github.com>
2024-02-18 21:31:08 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function Link(){
+alert("Parece que você é muito gay para comentar algo!");
+}
+/*function loadLink(id){
+var type = document.getElementById("filme");
+if(id>0){type.src="img2.jpg"
+};
+}*/
+
+
+//window.open("index2.html");
